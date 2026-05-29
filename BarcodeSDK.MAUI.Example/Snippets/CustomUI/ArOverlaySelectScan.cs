20f0d4cfccd237e875ece1ae545414d6c8b81624
scan-mayank <mayank.patil@apryse.com>
2026-05-29 15:57:28 +0200

    MAUI BS 9.0: Snippets - Fixed the breaking changes.

--- BarcodeSDK.MAUI.Example/Snippets/CustomUI/ArOverlaySelectScan.cs
+++ BarcodeSDK.MAUI.Example/Snippets/CustomUI/ArOverlaySelectScan.cs
@@ -7,16 +7,25 @@ public partial class Snippets
 {
     public static BarcodeScannerView EnableSelectScanArOverlay(BarcodeScannerView cameraView)
     {
-        cameraView.OverlayConfiguration = new SelectionOverlayConfiguration(
-            overlayFormat: BarcodeTextFormat.CodeAndType,
-            textColor: Colors.Yellow,
-            textContainerColor: Colors.Black,
-            strokeColor: Colors.Yellow,
-            highlightedStrokeColor: Colors.Red,
-            highlightedTextColor: Colors.Red,
-            highlightedTextContainerColor: Colors.Black,
-            polygonBackgroundColor: Colors.Transparent,
-            polygonBackgroundHighlightedColor: Colors.Transparent);
+        cameraView.OverlayConfiguration = new SelectionOverlayConfiguration
+        {
+            OverlayEnabled = true,
+            PolygonConfiguration = new OverlayPolygonConfiguration.Style
+            {
+                StrokeColor = Colors.Yellow,
+                HighlightedStrokeColor = Colors.Red,
+                PolygonColor = Colors.Transparent,
+                HighlightedPolygonColor = Colors.Transparent
+            },
+            TextConfiguration = new OverlayTextConfiguration.Style
+            {
+                TextFormat = BarcodeTextFormat.CodeAndType,
+                TextColor = Colors.Yellow,
+                TextContainerColor = Colors.Black,
+                HighlightedTextColor = Colors.Red,
+                HighlightedTextContainerColor = Colors.Black,
+            }
+        };
 
         cameraView.OnSelectBarcodeResult += (sender, items) =>
         {
